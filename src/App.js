b26f26ed5a85c8623f29692aace8e57e64f4610b
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2024-01-21 22:18:44 +0000

    Botones y tipografia

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { Add } from '@mui/icons-material';
+import TuneIcon from '@mui/icons-material/Tune';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { Button, Typography } from "@mui/material";
 
 function App() {
   return (
     <div>
-      Hello Material UI
+      <Button variant="contained" startIcon={<TuneIcon />} color='secondary'>Settings</Button>
+      <Button variant="contained" startIcon={<Add />} color='success'>Add New Post</Button>
+      <Button variant="outlined" disable startIcon={<TuneIcon />}>Outlined</Button>
+      <Typography variant='h1' component='p'> Titulo de la pagina</Typography>
     </div>
   );
 }
